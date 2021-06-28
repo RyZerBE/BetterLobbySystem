91c76e8b422f0a785c21ad977a9920c85bdf8dec
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-06-28 18:04:19 +0200

    optimize bad code xd

--- src/baubolp/ryzerbe/lobbycore/player/LobbyPlayer.php
+++ src/baubolp/ryzerbe/lobbycore/player/LobbyPlayer.php
@@ -382,7 +382,7 @@ class LobbyPlayer
                 $this->setNextLoginStreak(strtotime("next day"));
                 $this->setLastLoginStreak($now);
 
-                if (in_array($this->getLoginStreak(), [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200])) {
+                if ($this->getLoginStreak() % 5 === 0 && $this->getLoginStreak() != 0) {
                     if ($this->getPlayer() != null) {
                         CoinProvider::addCoins($this->getPlayer()->getName(), 1000);
                         $this->getPlayer()->sendMessage(Loader::PREFIX . LanguageProvider::getMessageContainer('lobby-loginstreak-get-coins', $this->getPlayer()->getName(), ['#coins' => 1000]));
