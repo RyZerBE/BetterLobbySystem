b6fab9365442c0233faba15d8f5bbe4018c60b0e
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-07-01 11:08:42 +0200

    implement test item rain cosmetic

--- src/baubolp/ryzerbe/lobbycore/cosmetic/category/type/ItemRainCategory.php
+++ src/baubolp/ryzerbe/lobbycore/cosmetic/category/type/ItemRainCategory.php
@@ -4,12 +4,13 @@ namespace baubolp\ryzerbe\lobbycore\cosmetic\category\type;
 
 use baubolp\ryzerbe\lobbycore\cosmetic\category\CosmeticCategory;
 use baubolp\ryzerbe\lobbycore\cosmetic\CosmeticManager;
+use baubolp\ryzerbe\lobbycore\cosmetic\type\itemrain\BedWarsItemRainCosmetic;
 
 class ItemRainCategory extends CosmeticCategory {
 
     public function loadCosmetics(): void{
         $cosmetics = [
-
+            new BedWarsItemRainCosmetic()
         ];
         foreach($cosmetics as $cosmetic) {
             $this->registerCosmetic($cosmetic);
---
+++ src/baubolp/ryzerbe/lobbycore/cosmetic/type/itemrain/BedWarsItemRainCosmetic.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace baubolp\ryzerbe\lobbycore\cosmetic\type\itemrain;
+
+
+use pocketmine\item\Item;
+
+class BedWarsItemRainCosmetic extends ItemRainCosmetic
+{
+
+    public function getName(): string
+    {
+        return "BedWars";
+    }
+
+    public function getPrice(): int
+    {
+        return 1000;
+    }
+
+    public function getIdentifier(): string
+    {
+        return "item_rain:bedwars";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getItems(): array
+    {
+        return [
+            Item::get(Item::STICK),
+            Item::get(Item::SANDSTONE),
+            Item::get(Item::WEB)
+        ];
+    }
+}
