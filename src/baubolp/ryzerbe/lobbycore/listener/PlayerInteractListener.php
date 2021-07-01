ee84a9dfc607b33f687dce237774424b95e5c470
Matze <47496465+Matze997@users.noreply.github.com>
2021-07-01 16:27:56 +0200

    Add "Not interactable blocks"

--- src/baubolp/ryzerbe/lobbycore/listener/PlayerInteractListener.php
+++ src/baubolp/ryzerbe/lobbycore/listener/PlayerInteractListener.php
@@ -5,16 +5,24 @@ namespace baubolp\ryzerbe\lobbycore\listener;
 
 
 use baubolp\ryzerbe\lobbycore\provider\ItemProvider;
+use pocketmine\block\Block;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerInteractEvent;
+use function in_array;
 
 class PlayerInteractListener implements Listener
 {
+    private const NOT_INTERACTABLE_BLOCKS = [
+        Block::DRAGON_EGG,
+        Block::CHEST,
+        Block::FURNACE
+    ];
 
     public function interact(PlayerInteractEvent $event)
     {
         $player = $event->getPlayer();
-        if (ItemProvider::execItem($player)) {
+        $block = $event->getBlock();
+        if (ItemProvider::execItem($player) || in_array($block->getId(), self::NOT_INTERACTABLE_BLOCKS)) {
             $event->setCancelled();
         }
     }
