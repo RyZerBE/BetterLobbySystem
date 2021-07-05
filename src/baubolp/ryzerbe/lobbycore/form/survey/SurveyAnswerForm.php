de6df7a9d5106c1122c180ac898125ecd6165083
Matze <47496465+Matze997@users.noreply.github.com>
2021-07-05 22:27:41 +0200

    ADd CustomForm example

--- src/baubolp/ryzerbe/lobbycore/form/survey/SurveyAnswerForm.php
+++ src/baubolp/ryzerbe/lobbycore/form/survey/SurveyAnswerForm.php
@@ -7,13 +7,14 @@ namespace baubolp\ryzerbe\lobbycore\form\survey;
 use baubolp\ryzerbe\lobbycore\Loader;
 use baubolp\ryzerbe\lobbycore\util\survey\Survey;
 use baubolp\ryzerbe\lobbycore\util\survey\SurveyAnswer;
-use pocketmine\form\CustomForm;
+use pocketmine\form\CustomForm as PMCustomForm;
 use pocketmine\form\CustomFormResponse;
 use pocketmine\form\element\Input;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
+use jojoe77777\FormAPI\CustomForm as CustomForm;
 
-class SurveyAnswerForm extends CustomForm
+class SurveyAnswerForm extends PMCustomForm
 {
 
     public function __construct(string $question, int $answerCount)
@@ -32,5 +33,28 @@ class SurveyAnswerForm extends CustomForm
             $survey->save();
             $player->sendMessage(Loader::PREFIX.TextFormat::GREEN."Deine Umfrage wurde erstellt.");
         });
+    }
+
+    /**
+     * @param Player $player
+     * @param string $question
+     * @param int $answerCount
+     */
+    public static function open(Player $player, string $question, int $answerCount): void {
+        $form = new CustomForm(function(Player $player, $data) use ($answerCount, $question): void {
+            if($data === null) return;
+            $surveyAnswers = [];
+            for($i = 0; $i < $answerCount; $i++) {
+                $surveyAnswers[] = $data[("answer_" . $i)];
+            }
+            $survey = new Survey($question, $surveyAnswers);
+            $survey->save();
+            $player->sendMessage(Loader::PREFIX.TextFormat::GREEN."Deine Umfrage wurde erstellt.");
+        });
+        $form->setTitle(TextFormat::AQUA.TextFormat::BOLD."Survey");
+        for($i = 0; $i < $answerCount; $i++) {
+            $form->addInput("Antwortmöglichkeit $i", "", "", "answer_" . $i);
+        }
+        $form->sendToPlayer($player);
     }
 }
