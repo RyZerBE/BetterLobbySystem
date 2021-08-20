ce9f16c35fe2e15e65953befd5c71ddb316968f3
Matze <47496465+Matze997@users.noreply.github.com>
2021-08-20 22:51:20 +0200

    Useless but cool

--- src/baubolp/ryzerbe/lobbycore/form/NavigatorForm.php
+++ src/baubolp/ryzerbe/lobbycore/form/NavigatorForm.php
@@ -3,7 +3,6 @@
 
 namespace baubolp\ryzerbe\lobbycore\form;
 
-
 use baubolp\ryzerbe\lobbycore\animation\AnimationProvider;
 use baubolp\ryzerbe\lobbycore\animation\type\NavigatorTeleportAnimation;
 use baubolp\ryzerbe\lobbycore\player\LobbyPlayerCache;
@@ -11,22 +10,37 @@ use baubolp\ryzerbe\lobbycore\provider\WarpProvider;
 use jojoe77777\FormAPI\SimpleForm;
 use pocketmine\Player;
 use pocketmine\utils\TextFormat;
+use function array_keys;
+use function in_array;
 
 class NavigatorForm
 {
     /** @var array */
     public static $games = [];
+    /** @var array  */
+    public static array $games_order = [];
 
     public static function open(Player $player)
     {
         $lobbyPlayer = LobbyPlayerCache::getLobbyPlayer($player);
         if ($lobbyPlayer === null) return;
-        $form = new SimpleForm(function (Player $player, $data) use ($lobbyPlayer): void {
+
+        $playername = $player->getName();
+        $games = self::$games_order[$playername] ?? array_keys(self::$games);
+
+        $form = new SimpleForm(function (Player $player, $data) use ($lobbyPlayer, $games): void {
             if ($data === null) return;
             $data = explode(":", $data);
 
             $warp = WarpProvider::getWarp($data[0]);
             if ($warp === null) return;
+
+            $games__ = [$data[1]];
+            foreach($games as $game) {
+                if(!in_array($game, $games__)) $games__[] = $game;
+            }
+            self::$games_order[$player->getName()] = $games__;
+
 
             if ($lobbyPlayer->isNavigatorAnimationEnabled()){
                 AnimationProvider::addActiveAnimation(new NavigatorTeleportAnimation($player, $warp, $data[1]));
@@ -36,7 +50,7 @@ class NavigatorForm
         });
 
         $form->setTitle(TextFormat::AQUA . TextFormat::BOLD . "Games");
-        foreach (array_keys(self::$games) as $game){
+        foreach ($games as $game){
             $icon = self::$games[$game]["icon"];
             $warpName = self::$games[$game]["warpName"];
 
