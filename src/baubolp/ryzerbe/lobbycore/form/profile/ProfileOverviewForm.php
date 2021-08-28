ac18fd8f313f91c12c25a297352b6bca87f2c2ff
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-08-28 15:13:05 +0200

    add icons to profile form

--- src/baubolp/ryzerbe/lobbycore/form/profile/ProfileOverviewForm.php
+++ src/baubolp/ryzerbe/lobbycore/form/profile/ProfileOverviewForm.php
@@ -22,11 +22,10 @@ class ProfileOverviewForm
 
             switch ($data) {
                 case "clans":
-                    $player->sendMessage(Ryzer::PREFIX.TextFormat::RED."Unser ClanSystem wird gerade neu entwickelt!");
-                    //$player->getServer()->dispatchCommand($player, "cui");
+                    $player->getServer()->dispatchCommand($player, "cui");
                     break;
                 case "friends":
-                    $player->sendMessage(Ryzer::PREFIX.TextFormat::RED."Soon! :(");
+                    $player->sendMessage(Ryzer::PREFIX.TextFormat::RED."Dieses Feature kommt in einem weiteren Update von uns!");
                     break;
                 case "discord":
                     $player->getServer()->dispatchCommand($player, "verify");
@@ -41,11 +40,11 @@ class ProfileOverviewForm
         });
 
         $form->setTitle(TextFormat::GREEN.TextFormat::BOLD."Your profile");
-        $form->addButton(TextFormat::AQUA."Discord Verification"."\n".TextFormat::GRAY."Touch to open", -1, "", "discord");
-        $form->addButton(TextFormat::RED."Settings"."\n".TextFormat::GRAY."Touch to open", -1, "", "settings");
-        $form->addButton(TextFormat::LIGHT_PURPLE."Network Level"."\n".TextFormat::GRAY."Touch to open", -1, "", "network_level");
-        $form->addButton(TextFormat::GREEN."Friends"."\n".TextFormat::GRAY."Touch to open", -1, "", "friends");
-        $form->addButton(TextFormat::YELLOW."Clans"."\n".TextFormat::GRAY."Touch to open", -1, "", "clans");
+        $form->addButton(TextFormat::AQUA."Discord Verification"."\n".TextFormat::GRAY."Touch to open", 1, "https://media.discordapp.net/attachments/412217468287713282/881160991809876019/discord-2474808-2056094.png?width=410&height=410", "discord");
+        $form->addButton(TextFormat::RED."Settings"."\n".TextFormat::GRAY."Touch to open", 1, "https://media.discordapp.net/attachments/412217468287713282/881163563354443806/99-998662_customizable-services-gear-settings-icon-clipart.png?width=242&height=242", "settings");
+        $form->addButton(TextFormat::LIGHT_PURPLE."Network Level"."\n".TextFormat::GRAY."Touch to open", 1, "https://media.discordapp.net/attachments/412217468287713282/881161840237879336/15_Top_Player_game_competition-512.png?width=410&height=410", "network_level");
+        $form->addButton(TextFormat::GREEN."Friends"."\n".TextFormat::GRAY."Touch to open", -1, "https://media.discordapp.net/attachments/412217468287713282/881164230009684029/friends.png?width=160&height=160", "friends");
+        $form->addButton(TextFormat::YELLOW."Clans"."\n".TextFormat::GRAY."Touch to open", 1, "https://media.discordapp.net/attachments/412217468287713282/881162752280903750/War_Leagues.png?width=144&height=144", "clans");
         $form->sendToPlayer($player);
     }
 }
