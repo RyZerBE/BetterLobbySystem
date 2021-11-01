5fbfe09003b0d7d44752d4d930f3ef93208056f2
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-11-01 16:18:43 +0100

    update scoreboard if coins are update

---
+++ src/baubolp/ryzerbe/lobbycore/listener/CoinUpdateListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace baubolp\ryzerbe\lobbycore\listener;
+
+use baubolp\core\event\PlayerCoinsAddEvent;
+use baubolp\core\event\PlayerCoinsRemoveEvent;
+use baubolp\ryzerbe\lobbycore\player\LobbyPlayerCache;
+use pocketmine\event\Listener;
+
+class CoinUpdateListener implements Listener {
+    /**
+     * @param PlayerCoinsAddEvent $event
+     */
+    public function add(PlayerCoinsAddEvent $event){
+        $lobbyPlayer = LobbyPlayerCache::getLobbyPlayer($event->getPlayer());
+        if($lobbyPlayer === null) return;
+        $lobbyPlayer->updateScoreboard();
+    }
+
+    /**
+     * @param PlayerCoinsRemoveEvent $event
+     */
+    public function remove(PlayerCoinsRemoveEvent $event){
+        $lobbyPlayer = LobbyPlayerCache::getLobbyPlayer($event->getPlayer());
+        if($lobbyPlayer === null) return;
+        $lobbyPlayer->updateScoreboard();
+    }
+}
