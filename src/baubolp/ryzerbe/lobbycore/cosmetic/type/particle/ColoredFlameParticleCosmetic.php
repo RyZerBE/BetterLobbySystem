daba113754d0fbfb5ee637876a0dec508fe0ac5f
Matze <47496465+Matze997@users.noreply.github.com>
2021-11-28 14:56:42 +0100

    ColoredFlameParticle: Fix color

--- src/baubolp/ryzerbe/lobbycore/cosmetic/type/particle/ColoredFlameParticleCosmetic.php
+++ src/baubolp/ryzerbe/lobbycore/cosmetic/type/particle/ColoredFlameParticleCosmetic.php
@@ -5,6 +5,7 @@ namespace baubolp\ryzerbe\lobbycore\cosmetic\type\particle;
 use pocketmine\level\particle\GenericParticle;
 use pocketmine\level\particle\Particle;
 use pocketmine\math\Vector3;
+use function mt_rand;
 
 class ColoredFlameParticleCosmetic extends ParticleCosmetic {
 
@@ -21,6 +22,6 @@ class ColoredFlameParticleCosmetic extends ParticleCosmetic {
     }
 
     public function getParticle(Vector3 $vector3): Particle{
-        return new GenericParticle($vector3, Particle::TYPE_COLORED_FLAME); //ParticleIds::COLORED_FLAME = 56; BedrockProtocol
+        return new GenericParticle($vector3, Particle::TYPE_COLORED_FLAME, mt_rand()); //ParticleIds::COLORED_FLAME = 56; BedrockProtocol
     }
 }
