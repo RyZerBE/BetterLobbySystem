98fe0978ead5cc8205bc1017ffbbd1193dff4bb6
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2021-12-03 17:04:31 +0100

    bruh fix for daily reward

--- src/baubolp/ryzerbe/lobbycore/form/DailyRewardForm.php
+++ src/baubolp/ryzerbe/lobbycore/form/DailyRewardForm.php
@@ -16,19 +16,19 @@ use function time;
 class DailyRewardForm extends MenuForm {
     public function __construct(LobbyPlayer $lobbyPlayer){
         $options = [];
-        if(date("Y-m-d". $lobbyPlayer->getDailyCoinBombTime()) === date("Y-m-d". time())){
+        if(date("Y-m-d", $lobbyPlayer->getDailyCoinBombTime()) === date("Y-m-d". time())){
             $options[] = new MenuOption(TextFormat::GREEN . "200 Coins" . "\n" . TextFormat::GRAY . "[" . TextFormat::YELLOW . "Click to get" . TextFormat::GRAY . "]", new FormIcon("textures/ui/MCoin", FormIcon::IMAGE_TYPE_PATH));
         }
         else{
             $options[] = new MenuOption(TextFormat::GREEN . "200 Coins" . "\n" . TextFormat::GRAY . "[" . TextFormat::RED . "You have to wait" . TextFormat::GRAY . "]", new FormIcon("textures/ui/MCoin", FormIcon::IMAGE_TYPE_PATH));
         }
-        if(date("Y-m-d". $lobbyPlayer->getDailyCoinBombTime()) === date("Y-m-d". time())){
+        if(date("Y-m-d", $lobbyPlayer->getDailyCoinBombTime()) === date("Y-m-d". time())){
             $options[] = new MenuOption(TextFormat::GREEN . "1x LottoTicket" . "\n" . TextFormat::GRAY . "[" . TextFormat::YELLOW . "Click to get" . TextFormat::GRAY . "]", new FormIcon("textures/items/map_mansion", FormIcon::IMAGE_TYPE_PATH));
         }
         else{
             $options[] = new MenuOption(TextFormat::GREEN . "1x LottoTicket" . "\n" . TextFormat::GRAY . "[" . TextFormat::RED . "You have to wait" . TextFormat::GRAY . "]", new FormIcon("textures/items/map_mansion", FormIcon::IMAGE_TYPE_PATH));
         }
-        if(date("Y-m-d". $lobbyPlayer->getDailyXPTime()) === date("Y-m-d". time())){
+        if(date("Y-m-d", $lobbyPlayer->getDailyXPTime()) === date("Y-m-d". time())){
             $options[] = new MenuOption(TextFormat::GREEN . "10 XP" . "\n" . TextFormat::GRAY . "[" . TextFormat::YELLOW . "Click to get" . TextFormat::GRAY . "]", new FormIcon("textures/items/experience_bottle", FormIcon::IMAGE_TYPE_PATH));
         }
         else{
@@ -46,7 +46,7 @@ class DailyRewardForm extends MenuForm {
             $options[] = new MenuOption(TextFormat::GREEN . "1x HypeTrain" . "\n" . TextFormat::GRAY . "[" . TextFormat::DARK_PURPLE . "YouTuber" . TextFormat::GRAY . "]", new FormIcon("textures/items/minecart_normal", FormIcon::IMAGE_TYPE_PATH));
         }
         if($lobbyPlayer->getPlayer()->hasPermission("lobby.hypetrain")){
-            if(date("Y-m-d". $lobbyPlayer->getDailyHypeTrainTime()) === date("Y-m-d". time())){
+            if(date("Y-m-d", $lobbyPlayer->getDailyHypeTrainTime()) === date("Y-m-d". time())){
                 $options[] = new MenuOption(TextFormat::GREEN . "1x HypeTrain" . "\n" . TextFormat::GRAY . "[" . TextFormat::YELLOW . "Click to get" . TextFormat::GRAY . "]", new FormIcon("textures/items/minecart_normal", FormIcon::IMAGE_TYPE_PATH));
             }
             else{
@@ -60,7 +60,7 @@ class DailyRewardForm extends MenuForm {
         parent::__construct(TextFormat::AQUA . "Daily Reward", "", $options, function(Player $player, int $selectedOption) use ($lobbyPlayer): void{
             switch($selectedOption){
                 case 0:
-                    if(date("Y-m-d". $lobbyPlayer->getDailyCoinTime()) === date("Y-m-d". time())){
+                    if(date("Y-m-d", $lobbyPlayer->getDailyCoinTime()) === date("Y-m-d". time())){
                         CoinProvider::addCoins($player->getName(), 200);
                         $lobbyPlayer->setDailyCoinTime($lobbyPlayer->getNextLoginStreak());
                         $player->playSound("random.levelup", 5.0, 1.0, [$player]);
@@ -71,7 +71,7 @@ class DailyRewardForm extends MenuForm {
                     }
                     break;
                 case 2:
-                    if(date("Y-m-d". $lobbyPlayer->getDailyXPTime()) === date("Y-m-d". time())){
+                    if(date("Y-m-d", $lobbyPlayer->getDailyXPTime()) === date("Y-m-d". time())){
                         $lobbyPlayer->asRyZerPlayer()->getNetworkLevel()->addXP(10, null);
                         $lobbyPlayer->setDailyXPTime($lobbyPlayer->getNextLoginStreak());
                         $player->playSound("random.levelup", 5.0, 1.0, [$player]);
@@ -82,7 +82,7 @@ class DailyRewardForm extends MenuForm {
                     }
                     break;
                 case 1:
-                    if(date("Y-m-d". $lobbyPlayer->getDailyLottoTicketTime()) === date("Y-m-d". time())){
+                    if(date("Y-m-d", $lobbyPlayer->getDailyLottoTicketTime()) === date("Y-m-d". time())){
                         LottoProvider::addTicket($lobbyPlayer);
                         $lobbyPlayer->setDailyLottoTicketTime($lobbyPlayer->getNextLoginStreak());
                         $player->playSound("random.levelup", 5.0, 1.0, [$player]);
@@ -93,7 +93,7 @@ class DailyRewardForm extends MenuForm {
                     }
                     break;
                 case 3:
-                    if(date("Y-m-d". $lobbyPlayer->getDailyCoinBombTime()) === date("Y-m-d". time()) && $player->hasPermission("lobby.coinbomb")){
+                    if(date("Y-m-d", $lobbyPlayer->getDailyCoinBombTime()) === date("Y-m-d". time()) && $player->hasPermission("lobby.coinbomb")){
                         $lobbyPlayer->addCoinbomb();
                         $lobbyPlayer->setDailyCoinBombTime($lobbyPlayer->getNextLoginStreak());
                         $player->playSound("random.levelup", 5.0, 1.0, [$player]);
@@ -104,7 +104,7 @@ class DailyRewardForm extends MenuForm {
                     }
                     break;
                 case 4:
-                    if(date("Y-m-d". $lobbyPlayer->getDailyHypeTrainTime()) === date("Y-m-d". time()) && $player->hasPermission("lobby.hypetrain")){
+                    if(date("Y-m-d", $lobbyPlayer->getDailyHypeTrainTime()) === date("Y-m-d". time()) && $player->hasPermission("lobby.hypetrain")){
                         $lobbyPlayer->addHypeTrains();
                         $lobbyPlayer->setDailyHypeTrainTime($lobbyPlayer->getNextLoginStreak());
                         $player->playSound("random.levelup", 5.0, 1.0, [$player]);
